2aab00cc10caf067b42e35f33347d6a652c3267c
Abu <efootball042605@gmail.com>
2025-12-18 11:52:25 +0500

    13-mashq

---
+++ task13.py
@@ -0,0 +1,9 @@
+qavat_soni = int(input("Qavat soni: "))
+
+umumiy_balandlik = 0
+
+for i in range(1, qavat_soni + 1):
+    balandlik = float(input(f'qavat balandligi {i}: '))
+    umumiy_balandlik += balandlik 
+
+print(umumiy_balandlik)
